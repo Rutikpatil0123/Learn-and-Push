06471955034320c3b138fa9970353db4bf134e83
Rutikpatil0123 <71816232+Rutikpatil0123@users.noreply.github.com>
2024-03-13 18:46:10 +0530

    Time: 475 ms (44.74%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-#SELECT * FROM Cinema where id % 2 = 1 and where description NOT LIKE 'boring' order by rating desc;
-select * from Cinema where id % 2 = 1 and description != 'boring' order by rating desc ;
+SELECT id,movie,description,rating FROM Cinema where id % 2 = 1 and description NOT LIKE 'boring' order by rating desc;
+#select * from Cinema where id % 2 = 1 and description != 'boring' order by rating desc ;
